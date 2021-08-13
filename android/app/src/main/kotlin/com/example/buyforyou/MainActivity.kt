1298ed149f70d5b51aed14a8eabdbeae67bb5c49
KeungXiongV <74707079+KeungXiongV@users.noreply.github.com>
2021-08-13 17:05:06 +0700

    firts

---
+++ android/app/src/main/kotlin/com/example/buyforyou/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.buyforyou
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/article_model.dart
@@ -0,0 +1,12 @@
+import 'package:buyforyou/model/source_model.dart';
+
+class Article {
+  Source source;
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String publishedAt;
+  String content;
+}
